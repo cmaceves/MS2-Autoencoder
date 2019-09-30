6ae36bfba819c770e5026ab853ab1c13d3cc8d4b
laserc <chc361@ucsd.edu>
2019-09-30 15:10:21 -0700

    added saves for the model

--- ms2-autoencoder.py
+++ ms2-autoencoder.py
@@ -47,11 +47,12 @@ autoencoder.fit(X_train, y_train,
                 validation_data=(X_test, y_test))
 
 predict_test = autoencoder.predict(X_test)
-
+#scale everything back 
 X_test_norm = X_test * normalize
 predict_test_norm = predict_test * normalize
-y_test_norm = y_test * normalize
+Y_test_norm = y_test * normalize
 
+#plot graph of a predicted and test case
 import matplotlib.pyplot as plt
 fig, axs = plt.subplots(3, 1, figsize=(20,20))
 i = 3
@@ -63,11 +64,24 @@ top_max = Y_test_norm[i]
 axs[0].plot(range(0, 2000), first)
 axs[0].set_ylim(bottom=0, top=np.amax(top_max), auto=True)
 print(np.amax(first))
-
 axs[1].plot(range(0, 2000), second)
 axs[1].set_ylim(bottom=0, top=np.amax(top_max), auto=True)
 print(np.amax(second))
-
 axs[2].plot(range(0, 2000), third)
 axs[2].set_ylim(bottom=0, top=np.amax(top_max), auto=True)
 print(np.amax(third))
+
+#save Model architecture to json
+json_model = autoencoder.to_json()
+json_file = open('autoencoder_json.json', 'w')
+json_file.write(json_model)
+#saving model architecture to yaml
+yaml_model = autoencoder.to_yaml()
+yaml_file = open('autoencoder_yaml.yaml', 'w')
+yaml_file.write(yaml_model)
+
+#save architeture and weights to hdf5
+autoencoder.save('autoencoder_model.h5')
+#loading model
+from keras.models import load_model
+model1 = load_model('autoencoder_model.h5')
