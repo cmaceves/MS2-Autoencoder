7b477fef0483405dcf168129a1527e16f7ff7908
laserc <chc361@ucsd.edu>
2019-11-06 14:45:59 -0800

    clean up

--- bin/data_concat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#python processing.py '/home/cchen/autoencode_output/' 'ready_array2.npz' --name '/home/cchen/MS2-Autoencoder/big_data.hdf5' --conv1d 'False'
-python processing.py '/home/cchen/autoencode_output/' 'ready_array2.npz' --name '/home/cchen/MS2-Autoencoder/big_data_conv1d.hdf5' --conv1d 'True'
